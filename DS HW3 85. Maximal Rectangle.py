f5b87a4117e12b6d43ab6e6cb2a9874bf69ed428
wencongli <liwencongpku@qq.com>
2019-12-18 23:01:26 -0500

    Update DS HW3 85. Maximal Rectangle.py

--- DS HW3 85. Maximal Rectangle.py
+++ DS HW3 85. Maximal Rectangle.py
@@ -1,4 +1,5 @@
 ‘’’
+Method 1: using DP
 Runtime: 204 ms, faster than 90.75% of Python3 online submissions for Maximal Rectangle.
 Memory Usage: 13.8 MB, less than 100.00% of Python3 online submissions for Maximal Rectangle.
 ‘’’
@@ -49,3 +50,53 @@ class Solution:
                     ret=max(ret, H[j]*(R[j]-L[j]))
                     
         return ret
+
+    
+'''
+method 2: using stack
+
+Runtime: 188 ms, faster than 98.54% of Python3 online submissions for Maximal Rectangle.
+Memory Usage: 13.8 MB, less than 100.00% of Python3 online submissions for Maximal Rectangle.
+
+'''
+
+class Solution:
+    def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        nrows=len(matrix)
+        if nrows==0: 
+            return 0
+        ncols=len(matrix[0])
+        if ncols==0:
+            return 0
+        
+        max_ret=0
+        # for each row, this is a regular "maximal rectangle" question
+        H=[0]*(ncols+1)
+        for k,row in enumerate(matrix):
+            # append '0' to the end of the list, 
+            # to force all of the element in stack to pop out
+            row=row+['0']
+            stack=[]
+            # "left" keeps track of the starting point of the stack
+            left=-1
+            for i in range(ncols+1):
+                if row[i]=='1':
+                    H[i]+=1
+                elif row[i]=='0':
+                    H[i]=0
+                while len(stack)>0 and H[i]<H[stack[-1]]:
+                    # if the new height smaller than the top of the stack
+                    # pop the stack and do evaluation
+                    # the matrix size is height*width
+                    ind=stack.pop()
+                    height=H[ind]
+                    if len(stack)==0:
+                        width=i-left-1
+                    else:
+                        width=i-stack[-1]-1
+                    max_ret=max(max_ret, height*width)
+                if H[i]>0:
+                    stack.append(i)
+                else:
+                    left=i
+        return max_ret
